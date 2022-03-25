a4cdae7b716fbab029b7f3e41edf7eb97fd8377f
Neha-081 <nehaprajapati1150@gmail.com>
2022-03-25 23:32:51 +0530

    updates

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -7,5 +7,5 @@ export const headerData = {
     title: "Full Stack Web Developer",
     desciption:"Dedicated and efficient aspiring full-stack web developer with a specialization in MERN stack. Very familiar with and passionate about web applications. Looking forward to new opportunities to enhance technical and business skills in a high-performance environment.",
     image: profilepic,
-    resumePdf: 'https://drive.google.com/drive/folders/13ogjGgCl4LKSBCrCliUmWbfhvEb_xTON?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1Guc_Q7hs5YLmnt2BCniLDgnn9_OIHJIh/view?usp=sharing'
 }
