38b793ee54a58e344ed817666b2a0a51ea01bf04
Neha Prajapati <87421798+Neha-081@users.noreply.github.com>
2022-06-16 13:57:31 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ import profilepic from '../assets/png/profilepic.jpg'
 export const headerData = {
     name: 'Neha Prajapati',
     title: "Full Stack Web Developer",
-    desciption:"Dedicated and efficient aspiring full-stack web developer with a specialization in MERN stack. Very familiar with and passionate about web applications. Looking forward to new opportunities to enhance technical and business skills in a high-performance environment.",
+    desciption:"Dedicated and efficient full-stack web developer with a specialization in MERN stack. Very familiar with and passionate about web applications. Looking forward to new opportunities to enhance technical and business skills in a high-performance environment.",
     image: profilepic,
     resumePdf: 'https://drive.google.com/file/d/1xaPoNt7YJJafI9HCZJZ7b80_X-agXVUM/view?usp=sharing'
 }
